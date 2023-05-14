5f180b160b2a54428db29eca807a3444af3aa03d
Exp <17885980+exp111@users.noreply.github.com>
2023-05-14 14:36:16 +0200

    spawnchest now kinda works (needs more data)
    dumpdropables

---
+++ MoreChatCommands/Commands/DumpDropable.cs
@@ -0,0 +1,63 @@
+﻿#if DEBUG
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreChatCommands
+{
+    public class DumpDropable : CustomDebugCmd
+    {
+        public override string Command => "dumpDropables";
+
+        public override string HelpText => "Dumps all dropables in the scene.";
+
+        public override string Usage => "/dumpDropable";
+
+        public override bool Cheat => true;
+
+        public override bool Run(string[] args)
+        {
+            // droptables:
+            // DropTable_Neutral_Cloth_Poor_1
+
+            // Check if droptable exists
+            var dropables = GameObject.FindObjectsOfType<Dropable>();
+
+            Dictionary<string, bool> done = new();
+            foreach (var dropable in dropables)
+            {
+                //MoreChatCommands.DebugLog($"dropable {dropable}");
+                var name = dropable.name;
+                if (done.TryGetValue(name, out var _))
+                    continue;
+
+                //TODO: handle multiple droptables per object.
+                //TODO: handle guaranteed/conditional drops
+                var obj = dropable.gameObject;
+                var droptableString = "null";
+                var droptable = obj.GetComponent<DropTable>();
+                if (droptable)
+                {
+                    var itemDrops = "null";
+                    if (droptable.m_itemDrops != null)
+                    {
+                        itemDrops = "";
+                        foreach (var drop in droptable.m_itemDrops)
+                        {
+                            itemDrops += $"new ItemDropChance(){{DropChance={drop.DropChance},MinDropCount={drop.MinDropCount},MaxDropCount={drop.MaxDropCount},ItemID={drop.DroppedItem.ItemID}}},";
+                        }
+                        itemDrops = $"new() {{{itemDrops}}}";
+                    }
+                    droptableString = $"new DropTable() {{MinNumberOfDrops={droptable.MinNumberOfDrops},MaxNumberOfDrops={droptable.MaxNumberOfDrops},m_itemDrops={itemDrops}}}";
+                }
+
+                var text = $"[\"{name}\"] = CreateDropable(\"{name}\", {droptableString}),";
+                MoreChatCommands.DebugLog(text);
+                done[name] = true;
+            }
+            ChatPrint($"Dumped {done.Count} dropables.");
+
+            return true;
+        }
+    }
+}
+#endif
--- MoreChatCommands/Commands/SpawnChest.cs
+++ MoreChatCommands/Commands/SpawnChest.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace MoreChatCommands
 {
@@ -12,6 +13,27 @@ namespace MoreChatCommands
 
         public override bool Cheat => true;
 
+        public static Dictionary<string, Dropable> Dropables = new()
+        {
+            ["exp111.DropTable_Neutral_Survival_Low_2"] = CreateDropable("DropTable_Neutral_Survival_Low_2", new DropTable() { MinNumberOfDrops = 2, MaxNumberOfDrops = 2, m_itemDrops = new() { new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000080 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000081 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000082 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000083 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000084 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000085 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000086 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000087 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000130 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000131 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000132 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000133 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000134 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000135 }, new ItemDropChance() { DropChance = 5, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000136 }, new ItemDropChance() { DropChance = 10, MinDropCount = 2, MaxDropCount = 5, ItemID = 6500090 }, new ItemDropChance() { DropChance = 10, MinDropCount = 2, MaxDropCount = 4, ItemID = 6400140 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 2120050 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 2130130 }, new ItemDropChance() { DropChance = 10, MinDropCount = 6, MaxDropCount = 12, ItemID = 5200001 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 3, ItemID = 6000070 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 3, ItemID = 6100010 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5600010 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5100010 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 2, ItemID = 6600020 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5100060 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5010100 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5000020 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 4200040 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 3, ItemID = 6500150 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000174 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 3, ItemID = 4100250 }, new ItemDropChance() { DropChance = 10, MinDropCount = 3, MaxDropCount = 10, ItemID = 9000010 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 5110003 }, } }),
+            ["exp111.DropTable_Neutral_Cloth_Poor_1"] = CreateDropable("DropTable_Neutral_Cloth_Poor_1", new DropTable() { MinNumberOfDrops = 1, MaxNumberOfDrops = 1, m_itemDrops = new() { new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000080 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000081 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000082 }, new ItemDropChance() { DropChance = 5, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000083 }, new ItemDropChance() { DropChance = 5, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000084 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000085 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000086 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000087 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000130 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000131 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000132 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000133 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000134 }, new ItemDropChance() { DropChance = 3, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000135 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000136 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000000 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000001 }, new ItemDropChance() { DropChance = 5, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000002 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000003 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000005 }, new ItemDropChance() { DropChance = 5, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000004 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000006 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000007 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000008 }, new ItemDropChance() { DropChance = 2, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000009 }, new ItemDropChance() { DropChance = 10, MinDropCount = 1, MaxDropCount = 1, ItemID = 3000174 }, } }),
+        };
+        public static Dictionary<int, Dropable[]> ChestDropablePrefabs = new()
+        {
+            [1001000] = new Dropable[] { Dropables["exp111.DropTable_Neutral_Cloth_Poor_1"], Dropables["exp111.DropTable_Neutral_Survival_Low_2"] },
+        };
+
+        public static Dropable CreateDropable(string name, DropTable droptable)
+        {
+            var obj = new GameObject(name);
+            var table = obj.AddComponent<DropTable>();
+            table.m_itemDrops = droptable.m_itemDrops;
+            table.MinNumberOfDrops = droptable.MinNumberOfDrops;
+            table.MaxNumberOfDrops = droptable.MaxNumberOfDrops;
+            var dropable = obj.AddComponent<Dropable>();
+            return dropable;
+        }
+
         public override bool Run(string[] args)
         {
             // chest ids:
@@ -21,7 +43,8 @@ namespace MoreChatCommands
             // 1000050 Ornate Chest B?
             // 1000060 Trog Chest
             // 1000070 Hollowed Trunk
-            if (args.Length < 1) // not enough args
+            // 1001000 Junk Pile
+            /*if (args.Length < 2) // not enough args
                 return false;
 
             // Parse item id
@@ -29,17 +52,40 @@ namespace MoreChatCommands
             {
                 ChatError($"Not a valid item id ({args[1]}).");
                 return true;
-            }
+            }*/
+
+            //var itemID = args[1];
+            var itemID = "1001000";
+            var id = 1001000; //TODO: remove
 
             // Check if item id exists
-            if (!ResourcesPrefabManager.ITEM_PREFABS.ContainsKey(args[1]))
+            if (!ResourcesPrefabManager.ITEM_PREFABS.ContainsKey(itemID))
             {
-                ChatError($"This item does not exist ({args[1]}).");
+                ChatError($"This item does not exist ({itemID}).");
+                return true;
+            }
+
+            var localPlayer = GetLocalPlayer();
+            if (localPlayer == null)
+            {
+                ChatError("Couldn't get local player.");
                 return true;
             }
 
             //GameObject.Instantiate();
-            Spawn.SpawnItem(id, 1);
+            Vector3 vector = localPlayer.CenterPosition + localPlayer.transform.forward * 1.5f;
+            if (Physics.Raycast(vector, localPlayer.transform.up * -1f, out _, 10f, Global.LargeEnvironmentMask))
+            {
+                Quaternion localRotation = localPlayer.transform.localRotation;
+                Item item = ItemManager.Instance.GenerateItemNetwork(id);
+                item.transform.position = vector;
+                item.transform.rotation = localRotation;
+                item.gameObject.AddComponent<SafeFalling>();
+                var container = item.gameObject.GetComponent<SelfFilledItemContainer>();
+                // add prefabs to chest
+                container.DropPrefabs = ChestDropablePrefabs[id];
+
+            }
             //TODO: spawn chest, add dropable with droptable in gameobject (or link it?)
             return true;
         }
